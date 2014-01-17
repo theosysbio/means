c94ac27c9a6b4194c1d5e9e42c9805f76a7c195d
Sisi <sissi_fan@hotmail.com>
2014-01-17 16:27:59 +0000

    added comment for unittest

--- MEA_package/ProgramFiles/test_fcount.py
+++ MEA_package/ProgramFiles/test_fcount.py
@@ -3,6 +3,11 @@ import unittest
 
 class FcountTestCase(unittest.TestCase):
     def test_fcount_2_2(self):
+        """
+        Given the number of moments is 2, the number of species is 2,
+        Then results of fcount.py should be 1) [[0,0],[0,1],[1,0],[2,0],[0,2],[1,1]], as the numbers sum to 2 or less
+        2) [[0,0],[2,0],[0,2],[1,1]] if delete the first orders
+        """
         fcount_result_without_firstorder, fcount_result = fcount(2,2)
         fcount_result_without_firstorder = sorted(fcount_result_without_firstorder)
         fcount_result = sorted(fcount_result)
