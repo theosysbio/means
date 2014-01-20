983bae3330fab89c6c834e7ffaaa1bba93bdc7ea
Quentin Geissmann <qgeissmann@gmail.com>
2014-01-20 13:13:21 +0000

    implement test for the final result of the program. Uses expected results stored as Inoutput/model_*.txt.out

---
+++ MEA_package/ProgramFiles/test_whole_prog.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+##  The expected out files are named`MODEL_FILENAME.txt.out`
+INOUT_DIR="../Inoutput/"
+OUT_FILE="ODEout.tmp"
+
+
+
+testModel(){
+	model=$1
+	good_result=$INOUT_DIR/$model.out
+
+	python runprogram.py --MEA --model=$INOUT_DIR/$model --ODEout=$OUT_FILE
+
+	tmp="$(mktemp)"
+	tmp2="$(mktemp)"
+	grep -v 'Time' $INOUT_DIR/$OUT_FILE > $tmp
+	grep -v 'Time' $good_result > $tmp2
+
+	diff_res=$(diff $tmp $tmp2)
+	if [ "$dif_res" != "" ] 
+	then
+		echo "$model FAILED!!"
+		return $FALSE
+	else
+		echo "$model OK"
+		return $TRUE
+	fi 
+
+	rm $tmp $tmp2
+}
+
+
+
+models=(model_p53.txt model_MM.txt model_dimer.txt model_Hes1.txt)
+for m in "${models[@]}"
+do
+	echo "testing $m:"
+	testModel $m
+done
