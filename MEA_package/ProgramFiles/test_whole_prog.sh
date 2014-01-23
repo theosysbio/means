acf1f3a49c363901c2d1acbf4fbc5943022352a9
Saulius Lukauskas <luksaulius@gmail.com>
2014-01-23 14:49:54 +0000

    modified the test_whole_prog script to support arbitrary options

--- MEA_package/ProgramFiles/test_whole_prog.sh
+++ MEA_package/ProgramFiles/test_whole_prog.sh
@@ -4,46 +4,52 @@
 INOUT_DIR="../Inoutput/"
 OUT_FILE="ODEout.tmp"
 
-
-
 testModel(){
-	model=$1
-	good_result=$INOUT_DIR/$model.out
+    parameters=$1
+    expected_output=$2
 
-	python runprogram.py --MEA --model=$INOUT_DIR/$model --ODEout=$OUT_FILE
-	
-	if [ ! -f $INOUT_DIR/$OUT_FILE ];
+    echo "> python runprogram $parameters --ODEout=$OUT_FILE"	
+	time python runprogram.py $parameters --ODEout=$OUT_FILE
+
+    if [ ! -f $INOUT_DIR/$OUT_FILE ];
 	then
-		echo "$model FAILED!!"
+		echo "Failed"
 		echo "No output was generated"
-		return $FALSE
+		return 0
 	fi
 
 	tmp="$(mktemp -t MEA.XXXXX)"
 	tmp2="$(mktemp -t MEA.XXXXX)"
 	grep -v 'Time' $INOUT_DIR/$OUT_FILE > $tmp
-	grep -v 'Time' $good_result > $tmp2
+	grep -v 'Time' $expected_output > $tmp2
 	
 	diff_res=$(diff $tmp $tmp2)
 	if [ -n "$diff_res" ] 
 	then
-		echo "Model $model FAILED!!"
-		return $FALSE
+        # Use sdiff if it exists in system
+        if [ -z `type -t sdiff` ]; then
+            diff='diff'
+        else
+            diff='sdiff'
+        fi
+        echo "> $sdiff $tmp $tmp2"
+	    $diff $tmp $tmp2	
+        echo "ERROR: outputs mismatch, see diff above"
+        return 1
 	else
-		echo "$model OK"
-		return $TRUE
+        echo "ALL OK"
+		rm $tmp $tmp2
+        return 0
 	fi 
-	
-	
-
-	rm $tmp $tmp2
 }
-
-
 
 models=(model_p53.txt model_MM.txt model_dimer.txt model_Hes1.txt)
 for m in "${models[@]}"
 do
 	echo "testing $m:"
-	testModel $m
+    testModel "--MEA --model=$INOUT_DIR/$m --ODEout=$OUT_FILE" "$INOUT_DIR/$m.out"
+    # Check if last command failed, and exit
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 done
