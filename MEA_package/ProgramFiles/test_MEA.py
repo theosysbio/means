5f389e0cb68a4f59537a31f831be925e5aea24ee
Quentin Geissmann <qgeissmann@gmail.com>
2014-01-24 12:33:28 +0000

    unitests for substitute_means_with_y 2

--- MEA_package/ProgramFiles/test_MEA.py
+++ MEA_package/ProgramFiles/test_MEA.py
@@ -1,10 +1,15 @@
 from MEA import make_damat
+from MEA import substitute_mean_with_y
+from MEA import substitute_raw_with_central
+from MEA import substitute_ym_with_yx
+
 import unittest
 from sympy import Matrix, diff, Symbol, Subs, Eq, var, simplify, S
 import re
 
 
 class MEATestCase(unittest.TestCase):
+
     def test_make_damat(self):
         """
         Given the number of moments is 3, the number of species is 2,
@@ -41,5 +46,69 @@ class MEATestCase(unittest.TestCase):
                                 [z] * 4]
 
         expected_damat[2,0] = [[z]*8] * 3
-
+        print "boooooooo1"
         self.assertEqual(expected_damat, result_damat)
+
+    def test_substitute_mean_with_y_simple_list(self):
+
+        """
+        Given the number of moments is 3, the number of species is 2,
+        And Given the propensities of the 3 reactions in `a_strings`,
+        Then results of make_damat() should produce the derivation "matrix" (`result_damat`)
+        exactly equal to the the expected one (`expected_damat`).
+
+        :return:
+        """
+
+        nvar = 2
+        mom = simplify([
+        "-2*x01*y_1 + x02 + y_1**2",
+        " -x01*y_0 - x10*y_1 + x11 + y_0*y_1",
+        " -2*x10*y_0 + x20 + y_0**2"])
+
+        expected_mom = simplify([
+        "-2*y_1*y_1 + x02 + y_1**2",
+        " -y_1*y_0 - y_0*y_1 + x11 + y_0*y_1",
+        "-2*y_0*y_0 + x20 + y_0**2"])
+
+
+        out_mom =  substitute_mean_with_y(mom, nvar)
+
+        self.assertEqual(Matrix(out_mom), Matrix(expected_mom))
+
+
+
+    def test_substitute_mean_with_y_list_of_list(self):
+
+        """
+        Given the number of moments is 3, the number of species is 2,
+        And Given the propensities of the 3 reactions in `a_strings`,
+        Then results of make_damat() should produce the derivation "matrix" (`result_damat`)
+        exactly equal to the the expected one (`expected_damat`).
+
+        :return:
+        """
+
+        nvar = 2
+        mom = simplify([
+            ["-2*c_2*x01*(-y_0 - y_1 + 301)", "-2*c_2"],
+            ["c_2*x10*(-y_0 - y_1 + 301)- x01", "x01 - c_0*y_0 - c_0*y_1"],
+            ["0","2 * x10 * (-c_0 * y_0 * (y_0 + y_1 - 181))"]
+        ])
+
+
+        expected_mom = simplify([
+            ["-2*c_2*y_1*(-y_0 - y_1 + 301)", "-2*c_2"],
+            ["c_2*y_0*(-y_0 - y_1 + 301)- y_1", "y_1 - c_0*y_0 - c_0*y_1"],
+            ["0", "2*y_0*(-c_0*y_0*(y_0 + y_1 - 181))"]
+        ])
+
+
+        out_mom =  substitute_mean_with_y(mom, nvar)
+
+        self.assertEqual(Matrix(out_mom), Matrix(expected_mom))
+
+
+
+
+
