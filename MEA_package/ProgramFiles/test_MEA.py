291b2410bfea7c0088c4d3f7e305e781e0b1d3ad
Quentin Geissmann <qgeissmann@gmail.com>
2014-01-27 17:47:13 +0000

    remover test for make_damat and update function names

--- MEA_package/ProgramFiles/test_MEA.py
+++ MEA_package/ProgramFiles/test_MEA.py
@@ -1,4 +1,4 @@
-from MEA import make_damat
+#from MEA import make_damat
 from MEA import substitute_mean_with_y
 from MEA import substitute_raw_with_central
 from MEA import substitute_ym_with_yx
@@ -11,43 +11,45 @@ import re
 
 class MEATestCase(unittest.TestCase):
 
-    def test_make_damat(self):
-        """
-        Given the number of moments is 3, the number of species is 2,
-        And Given the propensities of the 3 reactions in `a_strings`,
-        Then results of make_damat() should produce the derivation "matrix" (`result_damat`)
-        exactly equal to the the expected one (`expected_damat`).
+    ## we do not need this anymore, for we do not need damat
 
-        :return:
-        """
-
-        nMoments, nvariables = 3, 2
-
-        ymat = Matrix(nvariables, 1, lambda i, j : 0)
-        for i in range(nvariables):
-            ymat[i,0] = Symbol("y_%i" % i)
-
-        a_strings = ["c_0*y_0*(120-301+y_0+y_1)", "c_1*(301-(y_0+y_1))", "c_2*(301-(y_0+y_1))"]
-
-        nreactions = len(a_strings)
-
-        amat = Matrix(nMoments, 1, lambda i, j : simplify(a_strings[i]))
-
-        result_damat = make_damat(amat, nMoments, ymat)
-
-        expected_damat = Matrix(nMoments, 1, lambda i, j : 0)
-
-        z = S(0)
-        expected_damat[0,0] = [[diff(simplify(a_strings[0]), Symbol("y_0")), simplify("c_0*y_0")],
-                                [simplify("-c_1"), simplify("-c_1")],
-                                [simplify("-c_2"), simplify("-c_2")]]
-
-        expected_damat[1,0] = [[simplify("2*c_0"), simplify("c_0"), simplify("c_0"), z],
-                                [z] * 4,
-                                [z] * 4]
-
-        expected_damat[2,0] = [[z]*8] * 3
-        self.assertEqual(expected_damat, result_damat)
+    # def test_make_damat(self):
+    #     """
+    #     Given the number of moments is 3, the number of species is 2,
+    #     And Given the propensities of the 3 reactions in `a_strings`,
+    #     Then results of make_damat() should produce the derivation "matrix" (`result_damat`)
+    #     exactly equal to the the expected one (`expected_damat`).
+    #
+    #     :return:
+    #     """
+    #
+    #     nMoments, nvariables = 3, 2
+    #
+    #     ymat = Matrix(nvariables, 1, lambda i, j : 0)
+    #     for i in range(nvariables):
+    #         ymat[i,0] = Symbol("y_%i" % i)
+    #
+    #     a_strings = ["c_0*y_0*(120-301+y_0+y_1)", "c_1*(301-(y_0+y_1))", "c_2*(301-(y_0+y_1))"]
+    #
+    #     nreactions = len(a_strings)
+    #
+    #     amat = Matrix(nMoments, 1, lambda i, j : simplify(a_strings[i]))
+    #
+    #     result_damat = make_damat(amat, nMoments, ymat)
+    #
+    #     expected_damat = Matrix(nMoments, 1, lambda i, j : 0)
+    #
+    #     z = S(0)
+    #     expected_damat[0,0] = [[diff(simplify(a_strings[0]), Symbol("y_0")), simplify("c_0*y_0")],
+    #                             [simplify("-c_1"), simplify("-c_1")],
+    #                             [simplify("-c_2"), simplify("-c_2")]]
+    #
+    #     expected_damat[1,0] = [[simplify("2*c_0"), simplify("c_0"), simplify("c_0"), z],
+    #                             [z] * 4,
+    #                             [z] * 4]
+    #
+    #     expected_damat[2,0] = [[z]*8] * 3
+    #     self.assertEqual(expected_damat, result_damat)
 
     def test_substitute_mean_with_y_simple_list(self):
 
