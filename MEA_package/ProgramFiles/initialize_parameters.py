1e5a3af7de7b83f054702d719837c08a6d1757d9
Saulius Lukauskas <luksaulius@gmail.com>
2014-01-28 15:54:18 +0000

    deleted initialize_parameters.py as it is not used anywhere anymore

--- MEA_package/ProgramFiles/initialize_parameters.py
+++
@@ -1,9 +0,0 @@
-import sympy
-
-def initialize_parameters(nrateconstants,nvariables):
-
-    mumat = sympy.Matrix(nvariables, 1, lambda i,j:sympy.var('y_%d' % (i)))
-    Mumat = sympy.Matrix(nvariables, 1, lambda i,j:sympy.var('mu_%d' % (i)))
-    c = sympy.Matrix(nrateconstants, 1, lambda i,j:sympy.var('c_%d' % (i)))
-
-    return [mumat, Mumat, c]
