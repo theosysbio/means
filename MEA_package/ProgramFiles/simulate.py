eb14ae89b8996c798d177a9256d3586fffabb4ce
Saulius Lukauskas <luksaulius@gmail.com>
2014-01-30 18:08:07 +0000

    mini changes for simulate.py

--- MEA_package/ProgramFiles/simulate.py
+++ MEA_package/ProgramFiles/simulate.py
@@ -22,40 +22,24 @@ from CVODE import CVODE
 # corresponding list of names identifying the moments
 ########################################################################
 
-def simulate(mfkoutput, trajout, lib, t, param, initcond, maxorder):
-    """
+def parse_expansion_output(expansion_output_filename):
+    expansion_output_handle = open(expansion_output_filename, 'r')
+    try:
+        lines = expansion_output_handle.readlines()
+    finally:
+        expansion_output_handle.close()
 
-    :param mfkoutput: Name of output file from MFK/LNA (specified by --ODEout)
-    :param trajout: Name of output file for this function (where simulated trajectories would be stored, i.e. --simout)
-    :param lib: Name of the C file for solver (i.e. --library)
-    :param t: List of timepoints
-    :param param: List of kinetic parameters
-    :param initcond: List of initial conditions for each moment (in timeparameters file)
-    :param maxorder: Maximum order of moments to output to either plot or datafile. (Defaults to maximum order of moments)
-    :return:
-    """
-
-    # Get required info from mfkoutput, and if not all initial conditions
-    # are specified set the remainder to 0 as default
-
-    LHSfile = open(mfkoutput)
-    lines = LHSfile.readlines()
-    LHSfile.close()
-
-    simtype = lines[0].rstrip()
-    LHSindex = lines.index('LHS:\n')
-    cindex = lines.index('Constants:\n')
-    LHS = []
-    nspecies = 0
-    for i in range(LHSindex+1,cindex-1):
-        LHS.append(lines[i].strip())
+    simulation_type = lines[0].rstrip()
+    index_of_lhs_in_file = lines.index('LHS:\n')
+    index_of_constants_in_file = lines.index('Constants:\n')
+    lhs = []
+    number_of_species = 0
+    for i in range(index_of_lhs_in_file+1,index_of_constants_in_file-1):
+        lhs.append(lines[i].strip())
         if lines[i].startswith('y_'):
-            nspecies+=1
-    emptyic = np.zeros(len(LHS))
-    for i in range(len(initcond)):
-        emptyic[i]=initcond[i]
-    initcond = emptyic
-    
+            number_of_species+=1
+
+
     # Get list of moment names/IDs from mfkoutput to create labels for output data file
 
     momlistindex = lines.index('List of moments:\n')
@@ -65,41 +49,67 @@ def simulate(mfkoutput, trajout, lib, t, param, initcond, maxorder):
             moment_string = str(lines[i].strip('\n[]'))
             moment_list.append(moment_string)
 
+    return simulation_type, number_of_species, lhs, moment_list
+
+def simulate(expansion_output_filename, trajout, lib, t, param, initial_conditions, maxorder):
+    """
+
+    :param expansion_output_filename: Name of output file from MFK/LNA (specified by --ODEout)
+    :param trajout: Name of output file for this function (where simulated trajectories would be stored, i.e. --simout)
+    :param lib: Name of the C file for solver (i.e. --library)
+    :param t: List of timepoints
+    :param param: List of kinetic parameters
+    :param initial_conditions: List of initial conditions for each moment (in timeparameters file)
+    :param maxorder: Maximum order of moments to output to either plot or datafile. (Defaults to maximum order of moments)
+    :return:
+    """
+
+    # Get required info from the expansion output
+
+    # TODO: Replace this with Quentin's code.
+    simulation_type, number_of_species, lhs, moment_list = parse_expansion_output(expansion_output_filename)
+
+    # If not all intial conditions specified, append zeros to them
+    initial_conditions = initial_conditions[:]  # Make a copy before do
+    if len(initial_conditions) < len(lhs):
+        initial_conditions.extend([0.0] * (len(lhs) - len(initial_conditions)))
+
     ####################################################################
     # If simulation type is LNA ...
     ####################################################################
 
-    if simtype == 'LNA':
+
+    if simulation_type == 'LNA':
 
         # solve with selected parameters
+        soln = CVODE(lib, t, initial_conditions, param)
 
-        soln = CVODE(lib, t, initcond, param)
-        mu = [0]*nspecies
-        mu_t = [0]*len(t)
-        for i in range(0,nspecies):
-            mu_i = [0]*len(t)
+        mu = [0] * number_of_species
+        mu_t = [0] * len(t)
+        for i in range(0, number_of_species):
+            mu_i = [0] * len(t)
             for j in range(len(t)):
                 if i == 0:
-                    V = Matrix(nspecies,nspecies,lambda k,l:0)
-                    for v in range(2*nspecies):
-                        V[v] = soln[j,v+nspecies]
-                    mu_t[j] = np.random.multivariate_normal(soln[j,0:nspecies],V)
-                mu_i[j]=mu_t[j][i]
-            mu[i]=mu_i
+                    V = Matrix(number_of_species, number_of_species, lambda k, l:0)
+                    for v in range(2*number_of_species):
+                        V[v] = soln[j, v+number_of_species]
+                    mu_t[j] = np.random.multivariate_normal(soln[j, 0:number_of_species], V)
+                mu_i[j] = mu_t[j][i]
+            mu[i] = mu_i
 
         # write results to output file (Input file name, parameters, initial condtions,
         # timepoints, and trajectories for each moment)
 
         output = open(trajout, 'w')
 
-        output.write('>Input file: '+str(mfkoutput)+'\n>Parameters: '+str(param)+'\n>Starting values: '+str(initcond)+'\n')
+        output.write('>Input file: '+str(expansion_output_filename)+'\n>Parameters: '+str(param)+'\n>Starting values: '+str(initial_conditions)+'\n')
 
         output.write('time')
         for i in range(0, len(t)):
             output.write('\t'+str(t[i]))
         output.write('\n')
 
-        for m in range(0, nspecies):
+        for m in range(0, number_of_species):
             output.write(moment_list[m])
             for i in range(0, len(t)):
                 output.write('\t'+str(mu[m][i]))
@@ -107,17 +117,17 @@ def simulate(mfkoutput, trajout, lib, t, param, initcond, maxorder):
 
         output.close()
 
-        return [mu,moment_list]
+        return [mu, moment_list]
 
     ####################################################################
     # If simulation type is MEA ...
     ####################################################################
-    elif simtype == 'MEA':
+    elif simulation_type == 'MEA':
 
         # Solve equations for moment trajectories with given parameters
 
-        soln = CVODE(lib, t, initcond, param)
-        mu = [soln[:,i] for i in range(0, len(initcond))]
+        soln = CVODE(lib, t, initial_conditions, param)
+        mu = [soln[:,i] for i in range(0, len(initial_conditions))]
         
         # Check maximum order of moments to output to file/plot
         if maxorder == False:
@@ -139,12 +149,12 @@ def simulate(mfkoutput, trajout, lib, t, param, initcond, maxorder):
         output = open(trajout, 'w')
 
         initcond_str = ''
-        for i in initcond:
+        for i in initial_conditions:
             initcond_str += (str(i)+',')
         initcond_str = '['+initcond_str.rstrip(',')+']'
-        output.write('>\tInput file: '+str(mfkoutput)+'\n>\tParameters: '+str(param)+'\n>\tStarting values: '+initcond_str+'\n')
+        output.write('>\tInput file: '+str(expansion_output_filename)+'\n>\tParameters: '+str(param)+'\n>\tStarting values: '+initcond_str+'\n')
 
-        output.write('#\t'+str(len(t))+'\t'+str(nspecies)+'\t'+str(maxorder)+'\n')
+        output.write('#\t'+str(len(t))+'\t'+str(number_of_species)+'\t'+str(maxorder)+'\n')
 
         output.write('time')
         for i in range(0, len(t)):
