8325ab7b6ee2c484ff8c5f045310e2f891e6d7d9
Quentin Geissmann <qgeissmann@gmail.com>
2014-02-02 13:59:30 +0000

    fix central moment test

--- MEA_package/ProgramFiles/tests/test_centralmoments.py
+++ MEA_package/ProgramFiles/tests/test_centralmoments.py
@@ -9,23 +9,23 @@ class CentralMomentsTestCase(unittest.TestCase):
         mcounter = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [0, 0, 2], [0, 1, 1], [0, 2, 0],
                     [1, 0, 1], [1, 1, 0], [2, 0, 0]]
 
-        m = sympy.Matrix([map(sympy.sympify,
+        m = sympy.Matrix([
                               ['c_0 - c_1*y_0 - c_2*y_0*y_2/(c_6 + y_0)',
                                0,
                                0,
                                0,
                                'c_2*y_0/(c_6 + y_0)**2 - c_2/(c_6 + y_0)',
                                0,
-                               '-c_2*y_0*y_2/(c_6 + y_0)**3 + c_2*y_2/(c_6 + y_0)**2']),
-                          map(sympy.sympify, [
+                               '-c_2*y_0*y_2/(c_6 + y_0)**3 + c_2*y_2/(c_6 + y_0)**2'],
+                         [
                               'c_3*y_0 - c_4*y_1',
                               0,
                               0,
                               0,
                               0,
                               0,
-                              0]),
-                          map(sympy.sympify, [
+                              0],
+                          [
                               'c_4*y_1 - c_5*y_2',
                               0,
                               0,
@@ -33,14 +33,14 @@ class CentralMomentsTestCase(unittest.TestCase):
                               0,
                               0,
                               0
-                          ])])
-        species = sympy.Matrix(map(sympy.var, ['y_0', 'y_1', 'y_2']))
-        propensities = sympy.Matrix(map(sympy.sympify, ['c_0',
+                          ]])
+        species = sympy.Matrix(['y_0', 'y_1', 'y_2'])
+        propensities = sympy.Matrix(['c_0',
                                                         'c_1 * y_0',
                                                         'c_2*y_0*y_2/(c_6 + y_0)',
                                                         'c_3*y_0',
                                                         'c_4*y_1',
-                                                        'c_5*y_2']))
+                                                        'c_5*y_2'])
 
         stoichiometry_matrix = sympy.Matrix([[1, -1, -1, 0, 0, 0],
                                              [0, 0, 0, 1, -1, 0],
@@ -50,38 +50,38 @@ class CentralMomentsTestCase(unittest.TestCase):
         answer = eq_centralmoments(counter, mcounter, m, species, propensities, stoichiometry_matrix)
 
         # Note that this is a list, rather than a matrix
-        correct_answer = [
-            map(sympy.sympify, ['2*c_4*y_1*y_2 + c_4*y_1 - 2*c_5*y_2**2 + c_5*y_2 - 2*x_0_0_1*(c_4*y_1 - c_5*y_2)',
+        correct_answer = sympy.Matrix([
+            ['2*c_4*y_1*y_2 + c_4*y_1 - 2*c_5*y_2**2 + c_5*y_2 - 2*x_0_0_1*(c_4*y_1 - c_5*y_2)',
                                 '-2*c_5',
                                 '2*c_4',
                                 0,
                                 0,
                                 0,
-                                0]),
-            map(sympy.sympify, [
+                                0],
+            [
                 'c_3*y_0*y_2 + c_4*y_1**2 - c_4*y_1*y_2 - c_4*y_1 - c_5*y_1*y_2 - x_0_0_1*(c_3*y_0 - c_4*y_1) - x_0_1_0*(c_4*y_1 - c_5*y_2)',
                 0,
                 '-c_4 - c_5',
                 'c_4',
                 'c_3',
                 0,
-                0]),
-            map(sympy.sympify, ['2*c_3*y_0*y_1 + c_3*y_0 - 2*c_4*y_1**2 + c_4*y_1 - 2*x_0_1_0*(c_3*y_0 - c_4*y_1)',
+                0],
+            ['2*c_3*y_0*y_1 + c_3*y_0 - 2*c_4*y_1**2 + c_4*y_1 - 2*x_0_1_0*(c_3*y_0 - c_4*y_1)',
                                 0,
                                 0,
                                 '-2*c_4',
                                 0,
                                 '2*c_3',
-                                0]),
-            map(sympy.sympify, [
+                                0],
+            [
                 'c_0*y_2 - c_1*y_0*y_2 - c_2*y_0*y_2**2/(c_6 + y_0) + c_4*y_0*y_1 - c_5*y_0*y_2 - x_0_0_1*(c_0 - c_1*y_0 - c_2*y_0*y_2/(c_6 + y_0)) - x_1_0_0*(c_4*y_1 - c_5*y_2)',
                 '-c_2*y_0/(c_6 + y_0)',
                 0,
                 0,
                 '-c_1 + 2*c_2*y_0*y_2/(c_6 + y_0)**2 - 2*c_2*y_2/(c_6 + y_0) - c_5 - x_0_0_1*(c_2*y_0/(c_6 + y_0)**2 - c_2/(c_6 + y_0))',
                 'c_4',
-                '-c_2*y_0*y_2**2/(c_6 + y_0)**3 + c_2*y_2**2/(c_6 + y_0)**2 - x_0_0_1*(-c_2*y_0*y_2/(c_6 + y_0)**3 + c_2*y_2/(c_6 + y_0)**2)']),
-            map(sympy.sympify, [
+                '-c_2*y_0*y_2**2/(c_6 + y_0)**3 + c_2*y_2**2/(c_6 + y_0)**2 - x_0_0_1*(-c_2*y_0*y_2/(c_6 + y_0)**3 + c_2*y_2/(c_6 + y_0)**2)'],
+            [
                 'c_0*y_1 - c_1*y_0*y_1 - c_2*y_0*y_1*y_2/(c_6 + y_0) + c_3*y_0**2 - c_4*y_0*y_1 - x_0_1_0*(c_0 - c_1*y_0 - c_2*y_0*y_2/(c_6 + y_0)) - x_1_0_0*(c_3*y_0 - c_4*y_1)',
                 0,
                 '-c_2*y_0/(c_6 + y_0)',
@@ -89,8 +89,8 @@ class CentralMomentsTestCase(unittest.TestCase):
                 'c_2*y_0*y_1/(c_6 + y_0)**2 - c_2*y_1/(c_6 + y_0) - x_0_1_0*(c_2*y_0/(c_6 + y_0)**2 - c_2/(c_6 + y_0))',
                 '-c_1 + c_2*y_0*y_2/(c_6 + y_0)**2 - c_2*y_2/(c_6 + y_0) - c_4',
                 '-c_2*y_0*y_1*y_2/(c_6 + y_0)**3 + c_2*y_1*y_2/(c_6 + y_0)**2 + c_3 - x_0_1_0*(-c_2*y_0*y_2/(c_6 + y_0)**3 + c_2*y_2/(c_6 + y_0)**2)'
-            ]),
-            map(sympy.sympify, [
+            ],
+            [
                 '2*c_0*y_0 + c_0 - 2*c_1*y_0**2 + c_1*y_0 - 2*c_2*y_0**2*y_2/(c_6 + y_0) + c_2*y_0*y_2/(c_6 + y_0) - 2*x_1_0_0*(c_0 - c_1*y_0 - c_2*y_0*y_2/(c_6 + y_0))',
                 0,
                 0,
@@ -98,7 +98,7 @@ class CentralMomentsTestCase(unittest.TestCase):
                 '2*c_2*y_0**2/(c_6 + y_0)**2 - 4*c_2*y_0/(c_6 + y_0) - c_2*y_0/(c_6 + y_0)**2 + c_2/(c_6 + y_0) - 2*x_1_0_0*(c_2*y_0/(c_6 + y_0)**2 - c_2/(c_6 + y_0))',
                 0,
                 '-2*c_1 - 2*c_2*y_0**2*y_2/(c_6 + y_0)**3 + 4*c_2*y_0*y_2/(c_6 + y_0)**2 + c_2*y_0*y_2/(c_6 + y_0)**3 - 2*c_2*y_2/(c_6 + y_0) - c_2*y_2/(c_6 + y_0)**2 - 2*x_1_0_0*(-c_2*y_0*y_2/(c_6 + y_0)**3 + c_2*y_2/(c_6 + y_0)**2)'
-            ])]
+            ]])
 
 
         self.assertEqual(answer, correct_answer)
@@ -108,24 +108,24 @@ class CentralMomentsTestCase(unittest.TestCase):
 
         mcounter = [[0, 0], [0, 1], [1, 0], [0, 2], [1, 1], [2, 0]]
 
-        m = sympy.Matrix([map(sympy.sympify,
+        m = sympy.Matrix([
                               ['-c_0*y_0*(y_0 + y_1 - 181) + c_1*(-y_0 - y_1 + 301)',
                                0,
                                '-c_0',
-                               '-c_0']),
-                          map(sympy.sympify, [
+                               '-c_0'],
+                          [
                               'c_2*(-y_0 - y_1 + 301)',
                               0,
                               0,
-                              0]),
+                              0]
         ])
 
 
         species = sympy.Matrix(map(sympy.var, ['y_0', 'y_1']))
         number_of_moments = 2
-        propensities = sympy.Matrix(map(sympy.sympify, ['c_0*y_0*(y_0 + y_1 - 181)',
+        propensities = sympy.Matrix(['c_0*y_0*(y_0 + y_1 - 181)',
                                                         'c_1*(-y_0 - y_1 + 301)',
-                                                        'c_2*(-y_0 - y_1 + 301)']))
+                                                        'c_2*(-y_0 - y_1 + 301)'])
 
         stoichiometry_matrix = sympy.Matrix([[-1, 1, 0],
                                              [0, 0, 1]])
@@ -133,24 +133,23 @@ class CentralMomentsTestCase(unittest.TestCase):
         answer = eq_centralmoments(counter, mcounter, m, species, propensities, stoichiometry_matrix)
 
         # Note that this is a list, rather than a matrix
-        correct_answer = [
-            map(sympy.sympify, [
+        correct_answer = sympy.Matrix(
+            [ [
                 '-2*c_2*x_0_1*(-y_0 - y_1 + 301) + 2*c_2*y_1*(-y_0 - y_1 + 301) + c_2*(-y_0 - y_1 + 301)',
                 '-2*c_2',
                 '-2*c_2',
-                0]),
-            map(sympy.sympify, [
+                0],
+            [
                 '-c_0*y_0*y_1*(y_0 + y_1 - 181) + c_1*y_1*(-y_0 - y_1 + 301) - c_2*x_1_0*(-y_0 - y_1 + 301) + c_2*y_0*(-y_0 - y_1 + 301) - x_0_1*(-c_0*y_0*(y_0 + y_1 - 181) + c_1*(-y_0 - y_1 + 301))',
                 '-c_0*y_0 - c_1',
                 'c_0*x_0_1 - c_0*y_0 - c_0*y_1 - c_0*(y_0 + y_1 - 181) - c_1 - c_2',
                 'c_0*x_0_1 - c_0*y_1 - c_2'
-            ]),
-            map(sympy.sympify, [
+            ],
+            [
                 '-2*c_0*y_0**2*(y_0 + y_1 - 181) + c_0*y_0*(y_0 + y_1 - 181) + 2*c_1*y_0*(-y_0 - y_1 + 301) + c_1*(-y_0 - y_1 + 301) - 2*x_1_0*(-c_0*y_0*(y_0 + y_1 - 181) + c_1*(-y_0 - y_1 + 301))',
                 0,
                 '2*c_0*x_1_0 - 4*c_0*y_0 + c_0 - 2*c_1',
                 '2*c_0*x_1_0 - 4*c_0*y_0 - 2*c_0*(y_0 + y_1 - 181) + c_0 - 2*c_1'
-            ])
-        ]
+            ] ])
 
         self.assertEqual(answer, correct_answer)
