654a1cb9a0862217438588cee9bd404466bf1a37
Saulius Lukauskas <luksaulius@gmail.com>
2014-02-05 13:39:38 +0000

    added setup.py

---
+++ MEA_package/ProgramFiles/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='means',
+    version='trunk',
+    author='Theoretical Systems Biology Group',
+    author_email=None, # TODO: author email
+    packages=['means', 'means.approximation', 'means.inference', 'means.model', 'means.simulation', 'means.util'],
+    scripts=['bin/means.py'],
+    url=None, # TODO: url
+    license=None, # TODO: license
+    description='Moment Expansion Approximation method implementation with simulation and inference packages',
+    long_description=None, # TODO: readme.txt
+    install_requires=[
+        "numpy==1.8.0",
+        "sympy==0.7.2",
+        "matplotlib==1.1.1",
+        "scipy==0.13.2",
+        "Assimulo==trunk"
+    ],
+)
