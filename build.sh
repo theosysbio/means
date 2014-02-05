c511722b2544c6ebac34a70ad990e8d2dafb3abc
Saulius Lukauskas <luksaulius@gmail.com>
2014-02-05 14:34:45 +0000

    make sure to have a failsafe if we do not have package installed and want to uninstall it

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ pip install --quiet nosexcover
 pip install --quiet pylint
 
 # Uninstall previous version of our script
-pip uninstall means
+pip uninstall means || echo "Means not yet installed"
 # Install current version of our script
 pip install $CODE_DIR
 
