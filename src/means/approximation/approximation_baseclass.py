60dca0896e8cb182d0a30e468d74a00fd6bf5dff
Saulius Lukauskas <luksaulius@gmail.com>
2014-02-07 17:20:00 +0000

    a couple of changes to the comments

--- src/means/approximation/approximation_baseclass.py
+++ src/means/approximation/approximation_baseclass.py
@@ -3,14 +3,25 @@ from time import time
 
 class ApproximationBaseClass(object):
     """
-    A base class providing a framework to generate an `ODEProblem` from an explicit `Model` (and, possibly, parameters)
+    A base class providing a framework to generate an :class:`~means.approximation.ode_problem.ODEProblem`
+    from an explicit :class:`~means.model.model.Model`
+    (and, possibly, parameters)
     """
-    def __init__(self,model):
+    def __init__(self, model):
+        """
+        Initialise the approximator.
+
+        :param model: Model to approximate
+        :type model: :class:`~means.model.Model`
+        """
         self.__model = model
         self.time_last_run = None
 
     @property
     def model(self):
+        """
+        The model that is used in approximation
+        """
         return self.__model
 
     def _wrapped_run(self):
@@ -18,10 +29,10 @@ class ApproximationBaseClass(object):
 
     def run(self):
         """
-        This is a wrapper around `_run()` which allow to record the time taken by the last `_run()` call.
-        :param parameters: some undefined parameters that the derived class could need. For example,
-        for MEA, a maximal number of moment is needed.
-        :return: a `ODEProblem` object.
+        Perform the approximation. Return a constructed :class:`~means.approximation.ode_problem.ODEProblem` object.
+
+        :return: a constructed set of equations, encoded in :class:`~means.approximation.ode_problem.ODEProblem` object.
+        :rtype: :class:`~means.approximation.ode_problem.ODEProblem`
         """
         t0 = time()
         out_problem = self._wrapped_run()
