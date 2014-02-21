cfbccf9832004622bffc95b71616217a12273dfa
Saulius Lukauskas <luksaulius@gmail.com>
2014-02-21 16:49:18 +0000

    allow to skip/run the slower tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,12 @@ INOUT_DIR=$WORKSPACE/Inoutput
 pip install --quiet nosexcover
 pip install --quiet pylint
 
-nosetests --with-xcoverage --with-xunit --cover-package=means --cover-erase $CODE_DIR
+if [ "$1" == "with-slow-tests" ]; then
+   SLOWTESTS="--no-skip"
+else
+   SLOWTESTS=""
+fi
+nosetests $SLOWTESTS --with-xcoverage --with-xunit --cover-package=means --cover-erase $CODE_DIR
 cd $INOUT_DIR
 python -m means.tests.regression_tests --xunit | tee $WORKSPACE/regression_tests.xml
 
