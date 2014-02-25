061e302e6ceb22e6bed2cf1a511a6366542ec017
Saulius Lukauskas <luksaulius@gmail.com>
2014-02-25 19:16:37 +0000

    modified run_tests to first install IPython and pyzmq. Also make sure to store the results into separate file

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,9 @@ EXAMPLES_DIR=$WORKSPACE/prototypes/examples
 
 pip install --quiet nosexcover
 pip install --quiet pylint
+# Needed to run nosetests for notebooks
+pip install --quiet IPython
+pip install --quiet pyzmq
 
 if [ "$1" == "with-slow-tests" ]; then
    SLOWTESTS="--no-skip"
@@ -10,6 +13,6 @@ else
    SLOWTESTS=""
 fi
 nosetests $SLOWTESTS --with-xcoverage --with-xunit --cover-package=means --cover-erase $CODE_DIR
-nosetests --with-xunit $EXAMPLES_DIR
+nosetests --with-xunit --xunit-file="nosetests-notebooks.xml" $EXAMPLES_DIR
 cd $INOUT_DIR
 
