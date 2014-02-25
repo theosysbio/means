b0a19cc24fd895518bf76a2bfa15f9364bc41733
Quentin Geissmann <qgeissmann@gmail.com>
2014-02-25 00:09:56 +0000

    add plot to benchmarking script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-import sys
+import pylab as pl
 
 GIT_HEAD = "mea_performance"
 
@@ -20,22 +20,29 @@ def benchmark_means(max_order):
     out, err = process.communicate()
     n_eqs, t = out.rstrip().split(",")
     return int(n_eqs), round(float(t),3)
+def plot_all(dic):
+    pl.figure()
+    for i in dic:
+        pl.plot(d["n_eq"], d["dt"], linewidth=2.5, linestyle="-",label=d["legend"])
+    pl.legend(loc='upper left')
+    pl.show()
+
+
 
 
 to_benchmark = [
-    #{"git_tag":"means_no_optims", "legend":"my legend", "function":benchmark_means, "test_from": 2, "test_up_to": 4},
-    {"git_tag":"no_simplify_and_cache_diff", "legend":"my legend", "function":benchmark_means, "test_from": 2, "test_up_to": 5},
-    {"git_tag":"use_xreplace", "legend":"my legend", "function":benchmark_means, "test_from": 2, "test_up_to": 5},
-    {"git_tag":"only_necessary_moms", "legend":"my legend", "function":benchmark_means, "test_from": 1, "test_up_to": 5},
-    {"git_tag":"use_quick_solve", "legend":"my legend", "function":benchmark_means, "test_from": 1, "test_up_to": 6}
-    #{"git_tag":"custom_diff", "legend":"my legend", "function":benchmark_means, "test_from": 1, "test_up_to": 6},
+    #{"git_tag":"means_no_optims", "legend":"my legend", "function":benchmark_means, "test_from": 2, "test_up_to": 4, "dt":[], "n_eq":[]},
+    {"git_tag":"no_simplify_and_cache_diff", "legend":"my legend", "function":benchmark_means, "test_from": 2, "test_up_to": 5, "dt":[], "n_eq":[]},
+    {"git_tag":"use_xreplace", "legend":"my legend", "function":benchmark_means, "test_from": 2, "test_up_to": 5, "dt":[], "n_eq":[]},
+    {"git_tag":"only_necessary_moms", "legend":"my legend", "function":benchmark_means, "test_from": 1, "test_up_to": 5, "dt":[], "n_eq":[]},
+    {"git_tag":"use_quick_solve", "legend":"my legend", "function":benchmark_means, "test_from": 1, "test_up_to": 6, "dt":[], "n_eq":[]}
+    #{"git_tag":"custom_diff", "legend":"my legend", "function":benchmark_means, "test_from": 1, "test_up_to": 6, "dt":[], "n_eq":[]},
 ]
-for tb in to_benchmark:
-    tb["dt"] = []
-    tb["n_eq"] = []
+
 
 #means_no_optims no_simplify_and_cache_diff use_xreplace only_necessary_moms use_quick_solve custom_diff
 highest_max_order = max([tb["test_up_to"] for tb in to_benchmark])
+
 
 try:
     for max_order in range(1,highest_max_order + 1):
@@ -46,6 +53,7 @@ try:
             if tb["test_from"] <= max_order <= tb["test_up_to"]:
                 process = subprocess.Popen(['git', 'checkout', tb["git_tag"]], stderr=subprocess.PIPE,  )
                 out, err = process.communicate()
+                print err
                 # if err:
                 #     print "??"
                 #     sys.stderr("Failed to switch branch, the error was:")
@@ -59,6 +67,9 @@ try:
 
 finally:
     # We ensure we switch back to our branch
+    time.sleep(3)
     subprocess.Popen(['git', 'checkout', GIT_HEAD]).communicate()
+    plot_all(to_benchmark)
     exit(0)
+
 
