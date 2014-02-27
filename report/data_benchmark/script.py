40a06e90e2c6ea6ed5171bae55e21812782a3dbb
Quentin Geissmann <qgeissmann@gmail.com>
2014-02-27 19:13:40 +0000

    benchmark should close cleaneely on interupt

--- report/data_benchmark/script.py
+++ report/data_benchmark/script.py
@@ -51,7 +51,8 @@ class MyFigure(ReportUnit):
             time.sleep(1)
             self.out_object = to_benchmark
             os.remove("git_lock.tmp")
-            exit(1)
+            return
+        
 
     def git_swing(self, branch=GIT_HEAD):
         if branch:
