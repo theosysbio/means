44593896e15ba5cf28dc1fc3d791131e03d70c85
Sisi Fan <sissi_fan@hotmail.com>
2014-03-06 17:14:42 +0000

    SBML output successful, added one more bio-model

--- src/means/io/sbml.py
+++ src/means/io/sbml.py
@@ -10,7 +10,10 @@ def _parse_reaction(libsbml_reaction):
     reactants = {sympy.Symbol(r.getSpecies()): r.getStoichiometry() for r in libsbml_reaction.getListOfReactants()}
     products = {sympy.Symbol(p.getSpecies()): p.getStoichiometry() for p in libsbml_reaction.getListOfProducts()}
     kinetic_law = sympy.sympify(libsbml_reaction.getKineticLaw().getFormula())
+
+    # This would only work for SBML Level 3, prior levels do not have parameters within kinetic law
     parameters = sympy.symbols([p.getId() for p in libsbml_reaction.getKineticLaw().getListOfParameters()])
+
     return _Reaction(id_, reactants, products, kinetic_law, parameters)
 
 
@@ -32,23 +35,35 @@ def read_sbml(filename):
     species = sympy.symbols([s.getId() for s in sbml_model.getListOfSpecies()])
     reactions = map(_parse_reaction, sbml_model.getListOfReactions())
 
-    parameters = set()
+    # getListOfParameters is an attribute of the model for SBML Level 1&2
+    parameters = sympy.symbols([p.getId() for p in sbml_model.getListOfParameters()])
+
+    if not parameters:
+        track_local_parameters = True
+        parameters = set()
+    else:
+        track_local_parameters = False
 
     stoichiometry_matrix = np.zeros((len(species), len(reactions)), dtype=int)
     propensities = []
     for reaction_index, reaction in enumerate(reactions):
-        parameters.update(reaction.parameters)
+        if track_local_parameters:
+            parameters.update(reaction.parameters)
         reactants = reaction.reactants
         products = reaction.products
         propensities.append(reaction.propensity)
-
         for species_index, species_id in enumerate(species):
             net_stoichiometry = products.get(species_id, 0) - reactants.get(species_id, 0)
             stoichiometry_matrix[species_index, reaction_index] = net_stoichiometry
 
-    # sympy does not allow sorting its parameter lists by default,
-    # explicitly tell to sort by str representation
-    sorted_parameters = sorted(parameters, key=str)
+
+    if track_local_parameters:
+        # sympy does not allow sorting its parameter lists by default,
+        # explicitly tell to sort by str representation
+        sorted_parameters = sorted(parameters, key=str)
+    else:
+        sorted_parameters = parameters
+
     model = Model(sorted_parameters, species, propensities, stoichiometry_matrix)
 
     return model
