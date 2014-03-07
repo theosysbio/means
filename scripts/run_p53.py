2006d1437225ef048efc3a4ce3c7762c800a26b5
Quentin Geissmann <qgeissmann@gmail.com>
2014-03-07 16:38:24 +0000

    update scripts and report scripts

--- scripts/run_p53.py
+++ scripts/run_p53.py
@@ -1,10 +1,10 @@
 """
 This is just a simple script to run test during development
 """
-from means.approximation.mea.moment_expansion_approximation import run_mea
+from means.approximation.mea.moment_expansion_approximation import mea_approximation
 
 from means.examples.sample_models import MODEL_P53
 
-prob = run_mea(MODEL_P53, 4, "log-normal")
+prob = mea_approximation(MODEL_P53, 4, "log-normal")
 
 #"simple_closure_vs_legacy"
---
+++ scripts/ssa_test.py
@@ -0,0 +1,20 @@
+from means.simulation.ssa import SSASimulation, StochasticProblem
+import numpy as np
+from means.examples.sample_models import *
+
+
+>>> MODEL = MODEL_P53
+>>> RATES = [90, 0.002, 1.7, 1.1, 0.93, 0.96, 0.01]
+>>> INITIAL_CONDITIONS = [70, 30, 60]
+>>> TIME_RANGE = np.arange(0, 40, .1)
+>>> N_SSA = 16
+>>> problem = StochasticProblem(MODEL)
+>>> ssas = SSASimulation(problem,  random_seed=None)
+>>> mean_trajectories = ssas.simulate_system(RATES, INITIAL_CONDITIONS, TIME_RANGE, N_SSA, number_of_processes=4)
+
+
+import pylab as pl
+pl.figure()
+for tr in mean_trajectories:
+    tr.plot()
+pl.show()
