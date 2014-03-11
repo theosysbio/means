0ee540d6b344f7f5e01d3a97aaf507de785bcaf7
Saulius Lukauskas <luksaulius@gmail.com>
2014-03-11 11:33:09 +0000

    made average time to run into a multiprocessed script, enough with this shit

--- scripts/stability_analysis/average_time_to_run.py
+++ scripts/stability_analysis/average_time_to_run.py
@@ -7,7 +7,7 @@ import cPickle as pickle
 import timeit
 import hashlib
 import pandas as pd
-from collections import defaultdict
+import multiprocessing
 
 DISK_CACHE_DIRECTORY = '.cache'
 
@@ -28,6 +28,9 @@ SIMULATION_KWARGS_TO_TEST.append({'solver': 'ode15s', 'iter': 'Newton'})
 SIMULATION_KWARGS_TO_TEST.append({'solver': 'ode15s', 'rtol': 1e-6})
 # Euler with small step size
 SIMULATION_KWARGS_TO_TEST.append({'solver': 'euler', 'h': 0.001})
+
+# Let's make it run in parallel, this is nonsense otherwise
+N_PROCESSES = max(multiprocessing.cpu_count(), 9)  # 9, not 8 as we have 18 problems
 
 def _try_hipchat_notify(message, color='gray', *args, **kwargs):
     print message
@@ -117,11 +120,14 @@ def runtime_test_function(simulation, value, initial_conditions, timepoints):
             return False
     return f
 
+def _dict_to_str(dict_):
+    return '/'.join(['{0}={1!r}'.format(x, y) for x, y in sorted(dict_.items())])
+
 @disk_cached
-def _test_runtime_for(problem, number_of_runs=10, *args, **kwargs):
+def _test_runtime_for(problem, number_of_runs=10, **kwargs):
     RUNTIME_THRESHOLD = 3600 / 10.0
 
-    simulation = means.simulation.Simulation(problem, *args, **kwargs)
+    simulation = means.simulation.Simulation(problem, **kwargs)
     # Warm simulation instance up (cache the numerical evaluation routines)
 
     try:
@@ -132,7 +138,7 @@ def _test_runtime_for(problem, number_of_runs=10, *args, **kwargs):
 
     runtimes = {}
     for key, value in PARAMETERS.iteritems():
-        print "Timing {0}".format(key)
+        print "{1}\t{0}: pending".format(key, _dict_to_str(kwargs))
         timer = timeit.Timer(runtime_test_function(simulation, value, INITIAL_CONDITIONS, TIMEPOINTS))
         runtime_one = timer.timeit(number=1)
 
@@ -145,10 +151,40 @@ def _test_runtime_for(problem, number_of_runs=10, *args, **kwargs):
         runtime = timer.timeit(number=number_of_runs-1)
         runtime += runtime_one  # Add the first one again
         runtime /= float(number_of_runs)
-        print "Timing {0} result: {1}s per iteration".format(key, runtime)
+        print "{2}\t{0}: {1}s per iteration".format(key, runtime, _dict_to_str(kwargs))
         runtimes[key] = runtime
 
     return runtimes
+
+def compute_runtimes_for_problem(argument):
+
+    # Map joins the arguments into a tuple
+    description, problem = argument
+
+    max_order, closure = description
+    problem_runtimes = []
+    message_runtimes = []
+
+    for j, kwargs in enumerate(SIMULATION_KWARGS_TO_TEST):
+
+        parameter_runtimes = _test_runtime_for(problem, **kwargs)
+        for key, value in parameter_runtimes.iteritems():
+            d = {'max_order': max_order,
+                 'closure': closure}
+            d.update(kwargs)
+            d['parameter_set'] = key
+            d['runtime'] = value
+
+            problem_runtimes.append(d)
+            message_runtimes.append(", ".join(["{0}={1}".format(x, y) for x, y in sorted(d.items())
+                                               if x not in ['closure', 'max_order']]))
+
+    message = "Simulations for {0!r} ave now finished. " \
+              "Results: \n{1}".format(description, '\n'.join(message_runtimes))
+    _try_hipchat_notify(message)
+
+    return problem_runtimes
+
 
 @disk_cached
 def compute_runtimes():
@@ -156,35 +192,13 @@ def compute_runtimes():
 
     runtimes = []
 
-    number_of_problems = len(problems)
-    number_of_kwargs = len(SIMULATION_KWARGS_TO_TEST)
+    pool = multiprocessing.Pool(N_PROCESSES)
+    lists_of_runtimes = pool.map(compute_runtimes_for_problem, reversed(problems.items()))
 
-    for i, (description, problem) in enumerate(problems.iteritems()):
-        max_order, closure = description
-        problem_runtimes = []
-        message_runtimes = []
+    pool.close()
 
-        for j, kwargs in enumerate(SIMULATION_KWARGS_TO_TEST):
-            kwargs_key = ', '.join(map(lambda x: '{0}={1!r}'.format(x[0], x[1]), sorted(kwargs.items())))
-            print "[{0}/{1}] [{2}/{3}] {4}".format(i+1, number_of_problems, j+1, number_of_kwargs, kwargs_key)
-
-            parameter_runtimes = _test_runtime_for(problem, **kwargs)
-            for key, value in parameter_runtimes.iteritems():
-                d = {'max_order': max_order,
-                     'closure': closure}
-                d.update(kwargs)
-                d['parameter_set'] = key
-                d['runtime'] = value
-
-                problem_runtimes.append(d)
-                message_runtimes.append(", ".join(["{0}={1}".format(x, y) for x, y in sorted(d.items())
-                                                   if x not in ['closure', 'max_order']]))
-
-        message = "Simulations for {0!r} ({2}/{3}) have now finished. " \
-                  "Results: \n{1}".format(description, '\n'.join(message_runtimes), i+1, number_of_problems)
-        _try_hipchat_notify(message)
-
-        runtimes.append(problem_runtimes)
+    for runtime in lists_of_runtimes:
+        runtimes.extend(runtime)
 
     return pd.DataFrame(runtimes)
 
