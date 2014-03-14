e84dcd875a1164ed71b2754c513380b06faa1281
Saulius Lukauskas <luksaulius@gmail.com>
2014-03-14 11:51:10 +0000

    fixed reading of values for non-local parameter sets

--- src/means/io/sbml.py
+++ src/means/io/sbml.py
@@ -46,7 +46,8 @@ def _parse_reaction(libsbml_reaction):
     products = {sympy.Symbol(p.getSpecies()): p.getStoichiometry() for p in libsbml_reaction.getListOfProducts()}
     kinetic_law = _sympify_kinetic_law_formula(libsbml_reaction.getKineticLaw().getFormula())
     # This would only work for SBML Level 3, prior levels do not have parameters within kinetic law
-    parameters = sympy.symbols([p.getId() for p in libsbml_reaction.getKineticLaw().getListOfParameters()])
+    parameters = [(sympy.Symbol(p.getId()), p.getValue())
+                  for p in libsbml_reaction.getKineticLaw().getListOfParameters()]
 
     return _Reaction(id_, reactants, products, kinetic_law, parameters)
 
@@ -56,8 +57,8 @@ def read_sbml(filename):
     Read the model from a SBML file.
 
     :param filename: SBML filename to read the model from
-    :return: A fully created model instance
-    :rtype: ::class::`means.Model`
+    :return: A tuple, consisting of :class:`~means.core.model.Model` instance,
+             set of parameter values, and set of initial conditions variables.
     """
     import libsbml
 
@@ -72,23 +73,32 @@ def read_sbml(filename):
         raise ValueError('Cannot parse SBML model from {0!r}'.format(filename))
 
     species = sympy.symbols([s.getId() for s in sbml_model.getListOfSpecies()])
+    initial_conditions = [s.getInitialConcentration() for s in sbml_model.getListOfSpecies()]
     compartments = sympy.symbols([s.getId() for s in sbml_model.getListOfCompartments()])
+    compartment_sizes = [s.getSize() for s in sbml_model.getListOfCompartments()]
+
     reactions = map(_parse_reaction, sbml_model.getListOfReactions())
 
     # getListOfParameters is an attribute of the model for SBML Level 1&2
-    parameters = sympy.symbols([p.getId() for p in sbml_model.getListOfParameters()])
-
+    parameters_with_values = [(sympy.Symbol(p.getId()), p.getValue())
+                              for p in sbml_model.getListOfParameters()]
+    parameter_values = dict(parameters_with_values)
+    parameters = map(lambda x: x[0], parameters_with_values)
     if not parameters:
         track_local_parameters = True
         parameters = set()
+        parameter_values = {}
     else:
         track_local_parameters = False
+
 
     stoichiometry_matrix = np.zeros((len(species), len(reactions)), dtype=int)
     propensities = []
     for reaction_index, reaction in enumerate(reactions):
         if track_local_parameters:
-            parameters.update(reaction.parameters)
+            for param, value in reaction.parameters:
+                parameters.add(param)
+                parameter_values[param] = value
         reactants = reaction.reactants
         products = reaction.products
         propensities.append(reaction.propensity)
@@ -104,8 +114,12 @@ def read_sbml(filename):
     else:
         sorted_parameters = parameters
 
+    parameter_values_list = [parameter_values[p] for p in sorted_parameters]
+
     # We need to concatenate compartment names and parameters as in our framework we cannot differentiate the two
     compartments_and_parameters = compartments + sorted_parameters
+    parameter_values_list = compartment_sizes + parameter_values_list
+
     model = Model(compartments_and_parameters, species, propensities, stoichiometry_matrix)
 
-    return model
+    return model, parameter_values_list, initial_conditions
