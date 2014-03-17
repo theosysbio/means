2355f7724b2ec61329face259a5ec64113b539a0
Saulius Lukauskas <luksaulius@gmail.com>
2014-03-17 23:08:12 +0000

    multiprocessing

--- report/figure_hit_and_miss/trajectories_for_problem.py
+++ report/figure_hit_and_miss/trajectories_for_problem.py
@@ -5,6 +5,7 @@ import means.examples
 import argparse
 import numpy as np
 from datetime import datetime
+import multiprocessing
 
 def _deserialisable_problem(filename):
     return means.ODEProblem.from_file(filename)
@@ -24,10 +25,58 @@ def _parser():
     parser.add_argument('input', help='YAML file containing the inputs for generation,'
                                       'as well as their output files and solver options',
                         type=argparse.FileType('r'))
-
     parser.add_argument('-d', '--directory', help='Directory to put output files', default='.', type=_existing_dir)
 
+    parser.add_argument('--processes', help="Number of processes ot use", type=int, default=1)
+
     return parser
+
+def _pool_initialiser(problem_local, directory_local):
+    global problem, directory
+    problem = problem_local
+    # Warm-up the problem:
+    __ = problem.right_hand_side_as_function
+
+    directory = directory_local
+
+def _process_task(task):
+    global problem, directory
+
+    parameters = task['parameters']
+    initial_conditions = task['initial_conditions']
+    timepoints = task['timepoints']
+    if len(timepoints) == 3:
+        timepoints = np.arange(*timepoints)
+
+    output = os.path.join(directory, task['output'])
+
+    if os.path.exists(output):
+        print 'Skipping generation of {0} as it already exists'.format(output)
+        return
+
+    simulation_kwargs = task['simulation_kwargs']
+
+    simulation = means.Simulation(problem, **simulation_kwargs)
+
+    exception = None
+    start = datetime.now()
+    try:
+        trajectories = simulation.simulate_system(parameters=parameters,
+                                                  initial_conditions=initial_conditions,
+                                                  timepoints=timepoints)
+    except means.SolverException as e:
+        exception = e
+        trajectories = None
+    end = datetime.now()
+    time_taken = (end-start).total_seconds()
+
+    # Leave only first order trajectories
+    means_ = filter(lambda x: isinstance(x.description, means.Moment) and x.description.order == 1,
+                    trajectories)
+    means.io.to_file({'trajectories': means_,
+                      'exception': exception,
+                      'time_taken': time_taken}, output)
+
 
 def main():
 
@@ -39,44 +88,11 @@ def main():
 
     problem = options.problem
 
-    # Warm-up the problem:
-    problem.right_hand_side_as_function
-
-    for task in tasks:
-        parameters = task['parameters']
-        initial_conditions = task['initial_conditions']
-        timepoints = task['timepoints']
-        if len(timepoints) == 3:
-            timepoints = np.arange(*timepoints)
-
-        output = os.path.join(directory, task['output'])
-
-        if os.path.exists(output):
-            print 'Skipping generation of {0} as it already exists'.format(output)
-            continue
-
-        simulation_kwargs = task['simulation_kwargs']
-
-        simulation = means.Simulation(options.problem, **simulation_kwargs)
-
-        exception = None
-        start = datetime.now()
-        try:
-            trajectories = simulation.simulate_system(parameters=parameters,
-                                                      initial_conditions=initial_conditions,
-                                                      timepoints=timepoints)
-        except means.SolverException as e:
-            exception = e
-            trajectories = None
-        end = datetime.now()
-        time_taken = (end-start).total_seconds()
-
-        # Leave only first order trajectories
-        means_ = filter(lambda x: isinstance(x.description, means.Moment) and x.description.order == 1,
-                        trajectories)
-        means.io.to_file({'trajectories': means_,
-                          'exception': exception,
-                          'time_taken': time_taken}, output)
+    pool = multiprocessing.Pool(processes=options.processes,
+                                initializer=_pool_initialiser,
+                                initargs=(problem, directory))
+    __ = pool.map(_process_task, tasks)
+    pool.close()
 
 if __name__ == '__main__':
     main()
