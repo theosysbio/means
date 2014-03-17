3f84fd1714751f135dfa5ee1828aed5c2e99589a
Saulius Lukauskas <luksaulius@gmail.com>
2014-03-17 22:58:39 +0000

    made the script read from file and process to file checking if the result already exists

--- report/figure_hit_and_miss/trajectories_for_problem.py
+++ report/figure_hit_and_miss/trajectories_for_problem.py
@@ -1,3 +1,5 @@
+import os
+import yaml
 import means
 import means.examples
 import argparse
@@ -7,51 +9,74 @@ from datetime import datetime
 def _deserialisable_problem(filename):
     return means.ODEProblem.from_file(filename)
 
+def _existing_dir(directory):
+    if os.path.exists(directory):
+        if not os.path.isdir(directory):
+            raise ValueError('Directory provided {0!r} is not a directory'.format(directory))
+    else:
+        os.mkdir(directory)
+
+    return directory
+
 def _parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output', help="Output file to write trajectories to", required=True,
-                        type=argparse.FileType('w'))
     parser.add_argument('problem', help='Problem to simulate', type=_deserialisable_problem)
-    parser.add_argument('--parameters', help='Parameters for the generation',
-                        type=float, nargs='+', required=True)
-    parser.add_argument('--initial-conditions', help='Initial conditions for generation',
-                        type=float, nargs='+', required=True)
-    parser.add_argument('--last-timepoint', help="Last timepoint to simulate to, "
-                                                 "the simulation would run from 0 to last_timepoint with step size 0.1",
-                        type=float, required=True)
+    parser.add_argument('input', help='YAML file containing the inputs for generation,'
+                                      'as well as their output files and solver options',
+                        type=argparse.FileType('r'))
 
-    solver_options_group = parser.add_argument_group('Solver Options')
-    solver_options_group.add_argument('-s', '--solver', help="Solver to use", default='ode15s')
+    parser.add_argument('-d', '--directory', help='Directory to put output files', default='.', type=_existing_dir)
 
     return parser
 
 def main():
+
     parser = _parser()
     options = parser.parse_args()
+    directory = options.directory
 
-    simulation = means.Simulation(options.problem, solver=options.solver)
-    timepoints = np.arange(0, options.last_timepoint, 0.1)
-    exception = None
-    start = datetime.now()
-    try:
-        trajectories = simulation.simulate_system(parameters=options.parameters,
-                                                  initial_conditions=options.initial_conditions,
-                                                  timepoints=timepoints)
-    except means.SolverException as e:
-        exception = e
-        trajectories = None
-    end = datetime.now()
-    time_taken = (end-start).total_seconds()
+    tasks = yaml.load(options.input)
 
-    # Leave only first order trajectories
-    means_ = filter(lambda x: isinstance(x.description, means.Moment) and x.description.order == 1,
-                    trajectories)
-    means.io.to_file({'trajectories': means_,
-                      'exception': exception,
-                      'time_taken': time_taken}, options.output)
+    problem = options.problem
 
-    options.output.close()
+    # Warm-up the problem:
+    problem.right_hand_side_as_function
 
+    for task in tasks:
+        parameters = task['parameters']
+        initial_conditions = task['initial_conditions']
+        timepoints = task['timepoints']
+        if len(timepoints) == 3:
+            timepoints = np.arange(*timepoints)
+
+        output = os.path.join(directory, task['output'])
+
+        if os.path.exists(output):
+            print 'Skipping generation of {0} as it already exists'.format(output)
+            continue
+
+        simulation_kwargs = task['simulation_kwargs']
+
+        simulation = means.Simulation(options.problem, **simulation_kwargs)
+
+        exception = None
+        start = datetime.now()
+        try:
+            trajectories = simulation.simulate_system(parameters=parameters,
+                                                      initial_conditions=initial_conditions,
+                                                      timepoints=timepoints)
+        except means.SolverException as e:
+            exception = e
+            trajectories = None
+        end = datetime.now()
+        time_taken = (end-start).total_seconds()
+
+        # Leave only first order trajectories
+        means_ = filter(lambda x: isinstance(x.description, means.Moment) and x.description.order == 1,
+                        trajectories)
+        means.io.to_file({'trajectories': means_,
+                          'exception': exception,
+                          'time_taken': time_taken}, output)
 
 if __name__ == '__main__':
     main()
