9c196137611f0179c06b809df818e1c3f49befe9
Saulius Lukauskas <luksaulius@gmail.com>
2014-03-28 13:29:01 +0000

    hopefully fixed problems with interpolation of contour landscape, made it raise an error when it cannot handle it with instructions on how to install a package that can

--- src/means/inference/plotting.py
+++ src/means/inference/plotting.py
@@ -14,15 +14,47 @@ def plot_contour(x, y, z, x_label, y_label, ax=None, *args, **kwargs):
     if ax is None:
         ax = plt.gca()
 
-    xi = np.linspace(min(x), max(x), 100)
-    yi = np.linspace(min(y), max(y), 100)
+    x = np.array(x, dtype=np.float)
+    y = np.array(y, dtype=np.float)
+    z = np.array(z, dtype=np.float)
+
+    # Remove duplicates from x, y, z
+    seen_values = {}
+    for xi, yi, zi in zip(x, y, z):
+        try:
+            seen_values[xi, yi].append(zi)
+        except KeyError:
+            seen_values[xi, yi] = [zi]
+
+    new_x, new_y, new_z = [], [], []
+    for (xi, yi), zi_list in seen_values.iteritems():
+        new_x.append(xi)
+        new_y.append(yi)
+
+        # Use mean of distances. TODO: is this the right thing to do?
+        new_z.append(np.mean(zi_list))
+    new_x, new_y, new_z = np.array(new_x, dtype=np.float), np.array(new_y, dtype=np.float), \
+                          np.array(new_z, dtype=np.float)
+
+    # Replace x, y, z with new_x, new_y, new_z
+    x, y, z = new_x, new_y, new_z
+
+    xi = np.linspace(np.min(x), np.max(x), 100)
+    yi = np.linspace(np.min(y), np.max(y), 100)
 
     # Interpolate points to a grid
-    zi = griddata(x, y, z, xi, yi)
+    try:
+        zi = griddata(x, y, z, xi, yi)
+    except KeyError:
+        raise Exception("Got {0!r} while interpolating the landscape."
+                        "this may be due to `matplotlib.delaunay` package not being able to"
+                        "handle the interpolation. Try installing natgrid package via pip: "
+                        "`pip install git+git://github.com/matplotlib/natgrid.git`")
 
     # Plot contours
     ax.contourf(xi, yi, zi, *args, **kwargs)
     cs = ax.contour(xi, yi, zi, colors='k')
+
     # Some labels for contour lines
     ax.clabel(cs, inline=True)
 
@@ -44,12 +76,15 @@ def plot_2d_trajectory(x, y,
     if not start_and_end_locations_only:
         ax.plot(x, y, *args, **kwargs)
 
-    max_x = max(x)
-    min_x = min(x)
+    x = np.array(x)
+    y = np.array(y)
+
+    max_x = np.max(x)
+    min_x = np.min(x)
     padding_x = (max_x - min_x) * 0.1 / 2.0
 
-    max_y = max(y)
-    min_y = min(y)
+    max_y = np.max(y)
+    min_y = np.min(y)
     padding_y = (max_y - min_y) * 0.1 / 2.0
 
 
