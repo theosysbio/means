ec839abf72fad8b94719d0de9ecf8d109c1dee42
Saulius Lukauskas <luksaulius@gmail.com>
2014-04-04 19:14:34 +0100

    wrote about means.util

--- src/means/util/__init__.py
+++ src/means/util/__init__.py
@@ -1,3 +1,15 @@
+"""
+Utilities
+---------
+
+This package contains helper functions that did not fit into any other packages.
+These functions include helper functions for common operations when dealing with :mod:`sympy`,
+as well as functions that help with memoisation of CPU intensive function results.
+
+These functions are designed to be package-specific.
+The users of the software are generally discouraged to use any of these functions.
+"""
+
 import sympyhelpers
 from memoisation import MemoisableObject, memoised_property
 import memoisation
