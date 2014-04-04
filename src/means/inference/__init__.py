ad139ef7cc35ef19302956fa9c5bd70df98497aa
Saulius Lukauskas <luksaulius@gmail.com>
2014-04-04 18:34:19 +0100

    wrote about means.inference

--- src/means/inference/__init__.py
+++ src/means/inference/__init__.py
@@ -1,2 +1,18 @@
+"""
+Parameter Inference
+-----
+
+This part of the package provides utilities for parameter inference.
+Parameter inference will try to find the set of parameters which
+produces trajectories with minimal distance to the observed trajectories.
+Different distance functions are implemented (such as functions minimising the sum of squares error
+or functions based on parametric likelihood), but it is also possible to use custom distance functions.
+
+The package provides support for both inference from a single starting point (:class:`~means.inference.Inference`)
+or inference from random starting points (:class:`~means.inference.InferenceWithRestarts`).
+
+Some basic inference result plotting functionality is also provided by the package, see the documentation for
+:class:`~means.inference.InferenceResult` for more information on this.
+"""
 from inference import *
 from results import InferenceResult, InferenceResultsCollection
