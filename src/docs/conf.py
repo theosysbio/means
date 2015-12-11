170c59e9102ab5ee6b2ad5a9072d3312b64ef36d
Saulius Lukauskas <luksaulius@gmail.com>
2015-12-11 17:03:45 +0000

    removed yaml fromm mocks

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -316,5 +316,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'cython', 'sympy', 'matplotlib', 'yaml', 'assimulo', 'sbml']
+MOCK_MODULES = ['numpy', 'cython', 'sympy', 'matplotlib', 'assimulo', 'sbml']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
